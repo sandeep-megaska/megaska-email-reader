3fad365db7a690f0f7cd686bcc8a6deb123bd726
sandeep-megaska <sandeep.lalu@gmail.com>
2025-11-05 16:14:05 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return null;
+}
+
+export async function getServerSideProps() {
+  return {
+    redirect: { destination: '/settlements', permanent: false }
+  };
+}
