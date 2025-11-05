4fa83ff7609580422826177a613c8e585d918598
sandeep-megaska <sandeep.lalu@gmail.com>
2025-11-05 15:25:19 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true
+};
+module.exports = nextConfig;
